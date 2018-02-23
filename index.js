35a423a69ab6b69761f789029c385d45881c5711
Jesse Horwitz <therealjhorwitz@gmail.com>
2018-02-23 01:22:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function iLoveTheBeatles(number){
     strings.push("I love the Beatles!");
     i++;
   }
-  while(i < number && number < 15);
+  while(i <= number && number < 15);
   return strings;
 }
